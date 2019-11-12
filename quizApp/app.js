bd860218b3ea8e16b61269abebfff9d22e556dc6
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2019-11-13 01:56:47 +0530

    comments 6

--- quizApp/app.js
+++ quizApp/app.js
@@ -1,21 +1,47 @@
 const startButton= document.getElementById('start-btn')
 const questionContainerElement=document.getElementById('quection-container')
+const questionElement=document.getElementById('question')
+const answerButtonsElement=document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuectionIndex
+
 startButton.addEventListener('click',startGame)
 
 function startGame()
 {
      console.log('Started')
      startButton.classList.add('hide') 
-     const questionContainerElement.classList.remove('hide')
+     shuffledQuestions=questions.sort(()=>Math.random()- .5)
+     currentQuestionIndex=0
+      questionContainerElement.classList.remove('hide')
+      setNextQuestion()
 
 }
 
 function setNextQuestion()
 { 
-
+    showQuestion(shuffledQuestion[currentQuestionIndex])
 }
+
+function showQuestion(question)
+{
+    questionElement.innerText=questions.question
+}
+
 
 function slectAnswer()
 {
 
 }
+
+const questions=[
+{
+    question:'What is 2+2',
+    answers:[
+        {text:'4',correct:true },
+        {text:'22',correct:false}
+            
+    ]
+}
+
+]
